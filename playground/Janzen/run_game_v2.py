41a3fc111c68294376846513a7c4acc46f23f9bb
JanzenLiu <chengtsung_liu@yahoo.com>
2018-09-25 20:24:55 +0800

    FIX ModuleNotFoundError problem

--- playground/Janzen/run_game_v2.py
+++ playground/Janzen/run_game_v2.py
@@ -1,11 +1,12 @@
+import sys
 try:
     from .game_v2 import Game, GameEndCondition, PlayerType
-except (SystemError, ModuleNotFoundError) as e:
+except (ModuleNotFoundError if sys.version_info >= (3, 6) else SystemError) as e:
     from game_v2 import Game, GameEndCondition, PlayerType
 
 
 if __name__ == "__main__":
-    players = [(PlayerType.HUMAN, "Janzen"), (PlayerType.PC_NAIVE, "PC1")]
+    players = [(PlayerType.HUMAN, "Janzen"), (PlayerType.PC_FIRST_CARD, "PC1")]
     end_condition = GameEndCondition.ROUND_1
 
     game = Game(players=players, end_condition=end_condition, interval=2)
