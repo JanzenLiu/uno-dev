2417970c1dc1088fc28fc4bbc0e0695eed8782b7
JanzenLiu <chengtsung_liu@yahoo.com>
2018-10-01 23:38:26 +0800

    FIX typo

--- playground/Janzen/game_v2/game.py
+++ playground/Janzen/game_v2/game.py
@@ -48,7 +48,7 @@ class Game(object):
         assert isinstance(end_condition, GameEndCondition)
         assert isinstance(interval, (int, float)) or interval > 0
         # set logger
-        self.logger = UnoLogger(name="[Game]", color=Fore.LIGHTMAGENTA_EX)
+        self.logger = UnoLogger(name="Game", color=Fore.LIGHTMAGENTA_EX)
 
         # set players
         self.num_players = 0
