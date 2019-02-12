1628a68f85770a2fcf56a8b56c47ff932d58d4d9
JanzenLiu <chengtsung_liu@yahoo.com>
2019-02-12 23:58:41 +0800

    UPDATE .gitignore

--- playground/Janzen/run_game_v2_env_v1_dqn.py
+++ playground/Janzen/run_game_v2_env_v1_dqn.py
@@ -9,13 +9,16 @@ from collections import deque
 from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.models import Sequential
+import logging
+import argparse
 
 
 class DQNAgent:
     def __init__(self, state_size, action_size, hidden_sizes,
-                 discount_factor=0.49, learning_rate=0.001,
-                 batch_size=256, train_start=100, train_rl_start=1000,
-                 epsilon=1.0, epsilon_min=0.005, epsilon_steps=2000):
+                 discount_factor=0.99, learning_rate=0.001,  # 0.99, 0.001 originally
+                 batch_size=256, train_start=100, fix_q_start=0,
+                 epsilon=1.0, epsilon_min=0.005, epsilon_steps=50000,  # 1.0, 0.005, 50000
+                 memory_size=10000):
 
         self.state_size = state_size
         self.action_size = action_size
@@ -28,10 +31,11 @@ class DQNAgent:
         self.epsilon_decay = (self.epsilon - self.epsilon_min) / epsilon_steps
         self.batch_size = batch_size
         self.train_start = train_start
-        self.train_rl_start = train_rl_start
+        self.fix_q_start = fix_q_start
         self.training_rl = False
 
-        self.memory = deque(maxlen=10000)
+        self.memory_size = memory_size
+        self.memory = deque(maxlen=memory_size)
         self.model = self.build_model()
         self.target_model = self.build_model()
         self.update_target_model()
@@ -44,7 +48,7 @@ class DQNAgent:
             model.add(Dense(dim, input_dim=input_dim, activation='relu', kernel_initializer='he_uniform'))
             input_dim = dim
         model.add(Dense(self.action_size, input_dim=input_dim, activation='linear', kernel_initializer='he_uniform'))
-        model.summary()
+        # model.summary()
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
 
@@ -78,9 +82,9 @@ class DQNAgent:
             target = reward
 
             if not done:
-                if memory_size > self.train_rl_start:
+                if memory_size > self.fix_q_start:
                     if not self.training_rl:
-                        print("starting training RL.")
+                        print("starting fixed-q training.")
                         self.training_rl = True
                     target = reward + self.discount_factor * np.amax(self.target_model.predict(next_state)[0])
                 else:
@@ -99,13 +103,42 @@ class DQNAgent:
 
 
 if __name__ == "__main__":
-    EPISODES = 1000
+    EPISODES = 10000
 
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hidden_sizes', nargs='*', type=int, default=[])
+    parser.add_argument('--discount_factor', type=float, default=0.99)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--batch_size', type=int, default=256)
+    parser.add_argument('--train_start', type=int, default=100)
+    parser.add_argument('--fix_q_start', type=int, default=0)
+    parser.add_argument('--epsilon', type=float, default=1.0)
+    parser.add_argument('--epsilon_min', type=float, default=0.005)
+    parser.add_argument('--epsilon_steps', type=int, default=50000)
+    parser.add_argument('--log_id', '-i', type=int, required=True)
+    args = parser.parse_args()
+    kwargs = dict(args._get_kwargs())
+
+    # initialize logger
+    log_id = kwargs.pop('log_id')
+    assert log_id < 1000
+    filename = 'env-v1-dqn-{:0>3}.log'.format(log_id)
+    logger = logging.Logger("DQNLogger")
+    sh = logging.StreamHandler()
+    fh = logging.FileHandler(filename)
+    logger.addHandler(sh)
+    logger.addHandler(fh)
+
+    # log hyper-parameters
+    for k, v, in kwargs.items():
+        logger.info('{}={}'.format(k, v))
+
+    # initialize agent
     env = make_env(version=1)
     state_size = env.state_space_dim
     action_size = env.action_space_dim
-    hidden_sizes = []
-    agent = DQNAgent(state_size, action_size, hidden_sizes)
+    agent = DQNAgent(state_size, action_size, **kwargs)
 
     for i in range(EPISODES):
         state, done = env.reset()
@@ -120,9 +153,12 @@ if __name__ == "__main__":
 
         agent.update_target_model()
         win = env.players[0].num_cards == 0
-        print("Round {}: {} - {} cards left".format(i, 'win' if win else 'lose', env.players[0].num_cards))
+        if win:
+            logger.info("Round {}: win".format(i))
+        else:
+            logger.info("Round {}: lose - {} cards left".format(i, env.players[0].num_cards))
 
     env.end_round()
 
-    print(env.players[0].num_wins, env.players[0].cumulative_reward)
-    print(env.players[1].num_wins, env.players[1].cumulative_reward)
+    logger.info("win rate: {}, avg reward: {}".format(env.players[0].num_wins / EPISODES,
+                                                      env.players[0].cumulative_reward / EPISODES))
